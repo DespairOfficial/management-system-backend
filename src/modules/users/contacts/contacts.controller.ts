cc4e6eb0b32fc9d552692861a8e843a27c9314a4
despair <grobamist@gmail.com>
2023-06-01 17:31:13 +0300

    lint

--- src/modules/users/contacts/contacts.controller.ts
+++ src/modules/users/contacts/contacts.controller.ts
@@ -1,5 +1,5 @@
 import { UserInfoEntity } from './../entities/user-info.entity';
-import { Body, Controller, Patch, Req, UseGuards, BadRequestException, Get } from '@nestjs/common';
+import { Controller, Req, UseGuards, BadRequestException, Get } from '@nestjs/common';
 import { ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { Request } from 'express';
 import { BAD_TARGET } from '../../../constants';
