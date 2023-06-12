d269cacb39bc2caaec8e738f156c73d3a231abc0
despair <grobamist@gmail.com>
2023-06-13 00:40:24 +0300

    secure cookie

--- src/config/cookieOptions.ts
+++ src/config/cookieOptions.ts
@@ -3,7 +3,6 @@ import { CookieOptions } from 'express';
 export const refreshCookieOptions: CookieOptions = {
   httpOnly: true,
   // path: '/',
-  // secure: true,
-  // domain: '192.168.0.141',
-  // sameSite: 'strict',
+  secure: true,
+  sameSite: 'strict',
 };
