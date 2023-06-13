d03f674341741e16e5c9580f564ed2e6e03dbd8b
despair <grobamist@gmail.com>
2023-06-13 12:09:45 +0300

    sameSite: none in cookies

--- src/config/cookieOptions.ts
+++ src/config/cookieOptions.ts
@@ -2,7 +2,6 @@ import { CookieOptions } from 'express';
 
 export const refreshCookieOptions: CookieOptions = {
   httpOnly: true,
-  // path: '/',
   secure: true,
-  sameSite: 'strict',
+  sameSite: 'none',
 };
